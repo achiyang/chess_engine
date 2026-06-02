6f8d59f96c5562050fedeb5d0287c9bd3d11f54c
achiyang <sinhouse2@gmail.com>
2026-06-03 04:03:03 +0900

    PositionStatus & PositionStatusFlag 추가 PositionStatus() 추가

--- include/position.h
+++ include/position.h
@@ -16,8 +16,32 @@ typedef struct Position {
     int             fullmove_number;
 } Position;
 
+typedef uint32_t PositionStatus;
+
+enum PositionStatusFlag {
+    POSITION_STATUS_OK                     = 0,
+
+    POSITION_ERROR_NULL_POSITION           = 1U << 0,
+
+    POSITION_ERROR_INVALID_SIDE_TO_MOVE    = 1U << 1,
+    POSITION_ERROR_INVALID_CASTLING_RIGHTS = 1U << 2,
+    POSITION_ERROR_INVALID_EN_PASSANT      = 1U << 3,
+
+    POSITION_ERROR_INVALID_HALFMOVE_CLOCK  = 1U << 4,
+    POSITION_ERROR_INVALID_FULLMOVE_NUMBER = 1U << 5,
+
+    POSITION_ERROR_INVALID_PIECE           = 1U << 6,
+    POSITION_ERROR_PIECES_OVERLAP          = 1U << 7,
+    POSITION_ERROR_PIECES_MISMATCH         = 1U << 8,
+    POSITION_ERROR_OCCUPANCY_OVERLAP       = 1U << 9,
+    POSITION_ERROR_OCCUPANCY_MISMATCH      = 1U << 10,
+
+    POSITION_ERROR_WHITE_KING_COUNT        = 1U << 11,
+    POSITION_ERROR_BLACK_KING_COUNT        = 1U << 12
+};
+
 /* 
- * 이 모듈의 조작 함수들은 precondition을 검사하지 않는다.
+ * 이 모듈의 조작 함수들은 Precondition을 검사하지 않는다.
  * caller는 valid input과 Position invariant를 보장해야 한다.
  */
 
@@ -82,5 +106,7 @@ Piece position_remove_piece(Position *pos, Square square);
  * - capture는 처리하지 않는다.
  */
 void position_move_piece(Position *pos, Square from, Square to);
+
+PositionStatus position_validate(const Position *pos);
 
 #endif
--- src/position.c
+++ src/position.c
@@ -1,9 +1,11 @@
 #include "position.h"
 
+#include "bitboard.h"
+
 #include <string.h>
 
 void position_clear(Position *pos) {
-    for (Square square = A1; square < SQUARE_NB; ++square) {
+    for (Square square = 0; square < SQUARE_NB; ++square) {
         pos->board[square] = NO_PIECE;
     }
 
@@ -52,4 +54,124 @@ void position_move_piece(Position *pos, Square from, Square to) {
     pos->pieces[piece] ^= move_bb;
     pos->occupancies[color] ^= move_bb;
     pos->occupancies[BOTH] ^= move_bb;
+}
+
+static int bitboard_array_has_overlap(const Bitboard *bbs, int count) {
+    Bitboard seen = 0;
+
+    for (int i = 0; i < count; ++i) {
+        if (seen & bbs[i]) {
+            return 1;
+        }
+
+        seen |= bbs[i];
+    }
+
+    return 0;
+}
+
+PositionStatus position_validate(const Position *pos) {
+    PositionStatus status = POSITION_STATUS_OK;
+
+    if (!pos) {
+        return POSITION_ERROR_NULL_POSITION;
+    }
+
+    if (!side_is_valid(pos->side_to_move)) {
+        status |= POSITION_ERROR_INVALID_SIDE_TO_MOVE;
+    }
+
+    if (!castling_rights_is_valid(pos->castling_rights)) {
+        status |= POSITION_ERROR_INVALID_CASTLING_RIGHTS;
+    }
+
+    if (pos->en_passant != NO_SQUARE) {
+        if (!square_is_valid(pos->en_passant)) {
+            status |= POSITION_ERROR_INVALID_EN_PASSANT;
+        }
+        else if (pos->board[pos->en_passant] != NO_PIECE) {
+            status |= POSITION_ERROR_INVALID_EN_PASSANT;
+        }
+        else if (side_is_valid(pos->side_to_move)) {
+            Rank valid_rank =
+                pos->side_to_move == WHITE ?
+                    RANK_6 :
+                    RANK_3;
+
+            if (square_rank(pos->en_passant) != valid_rank) {
+                status |= POSITION_ERROR_INVALID_EN_PASSANT;
+            }
+        }
+    }
+
+    if (pos->halfmove_clock < 0) {
+        status |= POSITION_ERROR_INVALID_HALFMOVE_CLOCK;
+    }
+
+    if (pos->fullmove_number < 1) {
+        status |= POSITION_ERROR_INVALID_FULLMOVE_NUMBER;
+    }
+
+    Bitboard expected_pieces[PIECE_NB] = { 0 };
+    Bitboard expected_occupancies[COLOR_NB] = { 0 };
+
+    for (Square square = 0; square < SQUARE_NB; ++square) {
+        Piece piece = pos->board[square];
+
+        if (!piece_is_valid(piece) && piece != NO_PIECE) {
+            status |= POSITION_ERROR_INVALID_PIECE;
+            return status;
+        }
+
+        if (piece == NO_PIECE) {
+            continue;
+        }
+
+        Color color = piece_color_of(piece);
+        Bitboard bb = square_bb(square);
+
+        expected_pieces[piece] |= bb;
+        expected_occupancies[color] |= bb;
+    }
+
+    expected_occupancies[BOTH] =
+        expected_occupancies[WHITE] | expected_occupancies[BLACK];
+
+    if (bitboard_array_has_overlap(pos->pieces, PIECE_NB)) {
+        status |= POSITION_ERROR_PIECES_OVERLAP;
+        status |= POSITION_ERROR_PIECES_MISMATCH;
+    }
+    else {
+        for (Piece piece = 0; piece < PIECE_NB; ++piece) {
+            if (pos->pieces[piece] != expected_pieces[piece]) {
+                status |= POSITION_ERROR_PIECES_MISMATCH;
+                break;
+            }
+        }
+    }
+
+    if (bitboard_array_has_overlap(pos->occupancies, SIDE_NB)) {
+        status |= POSITION_ERROR_OCCUPANCY_OVERLAP;
+        status |= POSITION_ERROR_OCCUPANCY_MISMATCH;
+    }
+    else {
+        for (Color color = 0; color < SIDE_NB; ++color) {
+            if (pos->occupancies[color] != expected_occupancies[color]) {
+                status |= POSITION_ERROR_OCCUPANCY_MISMATCH;
+            }
+        }
+    }
+
+    if (pos->occupancies[BOTH] != expected_occupancies[BOTH]) {
+        status |= POSITION_ERROR_OCCUPANCY_MISMATCH;
+    }
+
+    if (bitboard_popcount(expected_pieces[WK]) != 1) {
+        status |= POSITION_ERROR_WHITE_KING_COUNT;
+    }
+    if (bitboard_popcount(expected_pieces[BK]) != 1) {
+        status |= POSITION_ERROR_BLACK_KING_COUNT;
+    }
+
+    return status;
 }
