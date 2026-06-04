56d6390bd2a4f6b8b4b236b2cc583b03c2f81555
achiyang <sinhouse2@gmail.com>
2026-06-04 11:32:43 +0900

    test case 추가

--- src/main.c
+++ src/main.c
@@ -5,27 +5,39 @@
 #include "fen.h"
 #include "attack.h"
 
+static const char *fen_list[] = {
+    STARTPOS_FEN,
+    "6k1/8/8/8/8/8/8/4rK2 w - - 0 1",
+    "6k1/8/8/8/7b/8/8/4K3 w - - 0 1",
+    "6k1/8/5n2/8/4K3/8/8/8 w - - 0 1",
+    "6k1/8/8/8/8/3p4/4K3/8 w - - 0 1",
+    "6k1/4r3/8/8/4P3/8/8/4K3 w - - 0 1"
+};
+
 int main(void) {
     attack_init();
 
     Position pos;
 
-    FenResult fen_result = fen_parse(&pos, STARTPOS_FEN);
-    if (fen_result != FEN_OK) {
-        printf("fen error: %d\n", fen_result);
-        return 1;
+    for (size_t i = 0; i < sizeof(fen_list) / sizeof(*fen_list); ++i) {
+        FenResult fen_result = fen_parse(&pos, fen_list[i]);
+        if (fen_result != FEN_OK) {
+            printf("fen error: %d\n", fen_result);
+            return 1;
+        }
+
+        PositionStatus pos_stat = position_validate(&pos);
+        if (pos_stat != POSITION_STATUS_OK) {
+            printf("position invalid: %u\n", (unsigned)pos_stat);
+            return 1;
+        }
+
+        position_print(&pos, stdout);
+
+        printf("white in check: %d\n", position_is_in_check(&pos, WHITE));
+        printf("black in check: %d\n", position_is_in_check(&pos, BLACK));
+        printf("\n\n");
     }
-
-    PositionStatus pos_stat = position_validate(&pos);
-    if (pos_stat != POSITION_STATUS_OK) {
-        printf("position invalid: %u\n", (unsigned)pos_stat);
-        return 1;
-    }
-
-    position_print(&pos, stdout);
-
-    printf("white in check: %d\n", position_is_in_check(&pos, WHITE));
-    printf("black in check: %d\n", position_is_in_check(&pos, BLACK));
 
     return 0;
 }
