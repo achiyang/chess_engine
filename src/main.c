3febfa40dd1ed4f65d5898c95363cad698610ac3
achiyang <sinhouse2@gmail.com>
2026-06-08 11:43:54 +0900

    test fen list 변경

--- src/main.c
+++ src/main.c
@@ -21,58 +21,184 @@ static const AttackTest attack_tests[] = {
         0
     },
     {
-        "rook check",
-        "6k1/8/8/8/8/8/8/4rK2 w - - 0 1",
+        "bare kings no check",
+        "6k1/8/8/8/8/8/8/4K3 w - - 0 1",
+        0,
+        0
+    },
+
+    /* rook / file / rank */
+    {
+        "black rook file check",
+        "4r1k1/8/8/8/8/8/8/4K3 w - - 0 1",
         1,
         0
     },
     {
-        "bishop check",
+        "black rook rank check",
+        "6k1/8/8/8/8/8/8/r3K3 w - - 0 1",
+        1,
+        0
+    },
+    {
+        "black rook blocked by white piece",
+        "4r1k1/8/8/8/4P3/8/8/4K3 w - - 0 1",
+        0,
+        0
+    },
+    {
+        "black rook blocked by black piece",
+        "4r1k1/8/8/8/4n3/8/8/4K3 w - - 0 1",
+        0,
+        0
+    },
+    {
+        "white rook file check",
+        "4k3/8/8/8/8/8/8/4R1K1 b - - 0 1",
+        0,
+        1
+    },
+
+    /* bishop / diagonal */
+    {
+        "black bishop diagonal check",
         "6k1/8/8/8/7b/8/8/4K3 w - - 0 1",
         1,
         0
     },
     {
-        "knight check",
-        "6k1/8/5n2/8/4K3/8/8/8 w - - 0 1",
+        "black bishop other diagonal check",
+        "6k1/8/8/8/1b6/8/8/4K3 w - - 0 1",
         1,
         0
     },
     {
-        "pawn check",
-        "6k1/8/8/8/8/3p4/4K3/8 w - - 0 1",
-        1,
-        0
-    },
-    {
-        "blocked rook",
-        "6k1/4r3/8/8/4P3/8/8/4K3 w - - 0 1",
+        "black bishop blocked by white piece",
+        "6k1/8/8/8/7b/6P1/8/4K3 w - - 0 1",
         0,
         0
     },
     {
-        "queen diagonal check",
+        "black bishop blocked by black piece",
+        "6k1/8/8/8/7b/6n1/8/4K3 w - - 0 1",
+        0,
+        0
+    },
+    {
+        "white bishop diagonal check",
+        "4k3/8/8/8/8/2B5/8/6K1 b - - 0 1",
+        0,
+        1
+    },
+
+    /* queen */
+    {
+        "black queen diagonal check",
         "6k1/8/8/8/8/8/2q5/4K3 w - - 0 1",
         1,
         0
     },
     {
-        "queen file check",
+        "black queen file check",
         "6k1/8/8/8/4q3/8/8/4K3 w - - 0 1",
         1,
         0
     },
     {
-        "white rook check",
-        "4k3/8/8/8/8/8/8/4R1K1 b - - 0 1",
+        "black queen rank check",
+        "6k1/8/8/8/8/8/8/q3K3 w - - 0 1",
+        1,
+        0
+    },
+    {
+        "black queen blocked",
+        "6k1/8/8/8/4q3/4P3/8/4K3 w - - 0 1",
+        0,
+        0
+    },
+
+    /* knight */
+    {
+        "black knight check",
+        "6k1/8/5n2/8/4K3/8/8/8 w - - 0 1",
+        1,
+        0
+    },
+    {
+        "black knight edge check",
+        "6k1/8/8/8/8/2n5/8/4K3 w - - 0 1",
+        1,
+        0
+    },
+    {
+        "white knight check",
+        "4k3/8/5N2/8/8/8/8/6K1 b - - 0 1",
         0,
         1
     },
     {
-        "white pawn check",
+        "knight no check",
+        "6k1/8/8/5n2/4K3/8/8/8 w - - 0 1",
+        0,
+        0
+    },
+
+    /* pawn */
+    {
+        "black pawn check from d3",
+        "6k1/8/8/8/8/3p4/4K3/8 w - - 0 1",
+        1,
+        0
+    },
+    {
+        "black pawn check from f3",
+        "6k1/8/8/8/8/5p2/4K3/8 w - - 0 1",
+        1,
+        0
+    },
+    {
+        "white pawn check from f7",
         "6k1/5P2/8/8/8/8/8/4K3 b - - 0 1",
         0,
         1
+    },
+    {
+        "white pawn check from d7",
+        "4k3/3P4/8/8/8/8/8/6K1 b - - 0 1",
+        0,
+        1
+    },
+    {
+        "pawn no check wrong direction",
+        "6k1/8/8/8/8/4p3/8/4K3 w - - 0 1",
+        0,
+        0
+    },
+
+    /* edge / corner sliding */
+    {
+        "corner rook check",
+        "k7/8/8/8/8/8/8/R6K b - - 0 1",
+        0,
+        1
+    },
+    {
+        "corner bishop check",
+        "k7/8/8/8/8/8/1B6/7K b - - 0 1",
+        0,
+        1
+    },
+    {
+        "long diagonal black bishop check",
+        "6k1/8/8/8/8/8/1b6/4K3 w - - 0 1",
+        0,
+        0
+    },
+    {
+        "long diagonal black bishop check from h4",
+        "6k1/8/8/8/7b/8/8/4K3 w - - 0 1",
+        1,
+        0
     }
 };
 
