9f823e3ba6cddeebfe04d545a9b43a59c8edd372
achiyang <sinhouse2@gmail.com>
2026-06-15 14:14:21 +0900

    movegen_generate_legal 추가

--- include/movegen.h
+++ include/movegen.h
@@ -5,7 +5,9 @@
 #include "movelist.h"
 
 /*
- * 이 모듈은 pseudo-legal move generation을 담당한다.
+ * 이 모듈은 move generation을 담당한다.
+ * pseudo-legal move generation과 legal move filtering을 제공한다.
+ *
  * 함수들은 별도의 runtime validation을 하지 않으며,
  * caller는 valid Position과 Position invariant를 보장해야 한다.
  */
@@ -30,5 +32,27 @@
  * - 현재 구현은 castling move를 생성하지 않는다.
  */
 void movegen_generate_pseudo_legal(const Position *pos, MoveList *list);
+
+/*
+ * Precondition:
+ * - pos != NULL
+ * - list != NULL
+ * - Position invariant가 유지되어 있어야 한다.
+ * - side_to_move가 valid side여야 한다.
+ * - 양쪽 king이 각각 정확히 하나 존재해야 한다.
+ * - en_passant state가 valid해야 한다.
+ * - attacks_init()이 먼저 호출되어 있어야 한다.
+ *
+ * Effect:
+ * - list를 clear한다.
+ * - 현재 side_to_move의 legal moves를 생성한다.
+ * - 내부적으로 make/unmake를 사용하지만, 함수 종료 시 pos는 원래 상태로 복구된다.
+ *
+ * Note:
+ * - pinned piece move, illegal king move, illegal en passant move를 제거한다.
+ * - 현재 pseudo-legal generator가 castling move를 생성하지 않으므로,
+ *   legal move list에도 castling move는 포함되지 않는다.
+ */
+void movegen_generate_legal(Position *pos, MoveList *list);
 
 #endif
